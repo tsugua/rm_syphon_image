e76fa4de488585f76600c616fd0bf00539b460b6
Shaun August <shaun@eoslightmedia.com>
2014-09-13 12:15:31 -0700

    Moved server to own method

--- Rakefile
+++ Rakefile
@@ -17,4 +17,5 @@ Motion::Project::App.setup do |app|
   app.embedded_frameworks << "vendor/Syphon.framework"
   app.vendor_project("vendor/ServerEOS", :xcode, :target => "ServerEOS", :products => ["libServerEOS.a"])
   app.bridgesupport_files << "vendor/ServerEOS/ServerEOS/BridgeSupport.bridgesupport"
+  app.info_plist['LSAppNapIsDisabled'] = 'true'
 end
--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -7,6 +7,7 @@ class AppDelegate
     
     buildMenu
     buildWindow
+    create_server
 
     start_timer    
   end
@@ -26,10 +27,40 @@ class AppDelegate
   end
 
   def start_timer
-    timer = EM.add_periodic_timer 0.001 do
-      # NSThread.detachNewThreadSelector('publish', toTarget: self, withObject: nil)  
-      publish
-   end
+    # timer = EM.add_periodic_timer 0.001 do
+      # publish
+   # end 
+   
+      @lameRenderingTimer = NSTimer.timerWithTimeInterval(1.0/60.0, target:self, selector:'publish', userInfo:nil, repeats:true)
+      # lameRenderingTimer.retain
+      NSRunLoop.currentRunLoop.addTimer(@lameRenderingTimer, forMode:NSRunLoopCommonModes)
+      
+
+  end
+  
+  def create_server
+    
+		@attrs			  = Pointer.new_with_type('I', 11)
+		@attrs[0]		= NSOpenGLPFADoubleBuffer
+		@attrs[1]		= NSOpenGLPFAAccelerated
+		@attrs[2]		= NSOpenGLPFADepthSize
+		@attrs[3]		= 24
+		@attrs[4]		= NSOpenGLPFAMultisample
+		@attrs[5]		= NSOpenGLPFASampleBuffers
+		@attrs[6]		= NSOpenGLPFAColorSize
+		@attrs[7]		= 1
+		@attrs[8]		= NSOpenGLPFASamples
+		@attrs[9]		= 4
+		@attrs[10]	  = 0
+
+    
+    @pf = NSOpenGLPixelFormat.alloc.initWithAttributes(@attrs)
+    
+    @glContext = NSOpenGLContext.alloc.initWithFormat(@pf, shareContext:nil)
+    @glContext.makeCurrentContext
+
+    @syphonServer = ServerEOS.alloc.createSyphonServer("test", withContext: @glContext)
+    
   end
   
   def save_image
@@ -41,45 +72,8 @@ class AppDelegate
   end
   
   def publish
-    
-		attrs			  = Pointer.new_with_type('I', 11)
-		attrs[0]		= NSOpenGLPFADoubleBuffer
-		attrs[1]		= NSOpenGLPFAAccelerated
-		attrs[2]		= NSOpenGLPFADepthSize
-		attrs[3]		= 24
-		attrs[4]		= NSOpenGLPFAMultisample
-		attrs[5]		= NSOpenGLPFASampleBuffers
-		attrs[6]		= NSOpenGLPFAColorSize
-		attrs[7]		= 1
-		attrs[8]		= NSOpenGLPFASamples
-		attrs[9]		= 4
-		attrs[10]	  = 0
 
-    
-    pf = NSOpenGLPixelFormat.alloc.initWithAttributes(attrs)
-    
-    #this doesn't work because we haven't validated the pixel format... Do we need to do this?
-    # unless pf
-    #   p "Could not create pixel format, falling back to simpler pixel format"
-    #
-    #       simpleattr      = Pointer.new_with_type('I', 3)
-    #       simpleattr[0]    = NSOpenGLPFADoubleBuffer
-    #       simpleattr[1]    = NSOpenGLPFAAccelerated
-    #       simpleattr[2]    = 0
-    #
-    #   pf = NSOpenGLPixelFormat.alloc.initWithAttributes(simpleattr)
-    # end
-    #
-    # p "pf is #{pf}"
-    # unless pf
-    #     p "Could not create pixel format, bailing"
-    #     # NSApp.terminate(self)
-    # end
-    
-    glContext = NSOpenGLContext.alloc.initWithFormat(pf, shareContext:nil)
-    glContext.makeCurrentContext
-
-    @syphonServer = ServerEOS.alloc.createSyphonServer("test", withContext: glContext) unless @syphonServer
+    @glContext.makeCurrentContext    
 
     @imageView = @view.get_imageview
     anImage = @imageView.image
@@ -90,27 +84,27 @@ class AppDelegate
     bytesPerRow =((bitsPerComponent * @imageView.image.size.width) / 8) * componentsPerPixel
     colorSpace = CGColorSpaceCreateDeviceRGB()
     
-    # p "image size:  #{anImage.size.width}, #{anImage.size.height}"
-    
     
     imageRep = NSBitmapImageRep.alloc.initWithData(anImage.TIFFRepresentation)
     pixelData = imageRep.CGImage
-    
+        
     gtx = CGBitmapContextCreate(nil, anImage.size.width, anImage.size.height, bitsPerComponent, bytesPerRow, colorSpace, KCGImageAlphaPremultipliedLast)
     
     CGContextDrawImage(gtx, CGRectMake(0, 0, 1132, 994), pixelData)
     CGContextFlush(gtx)
+    
 
     texture = GLKTextureLoader.textureWithCGImage(pixelData, options:nil, error:nil)
-
-    # p "texture: #{texture.name}, #{texture.width}, #{texture.height}"
+    if texture
     
-    if @syphonServer.hasClients
-      @syphonServer.publishFrameTexture(texture.name,
+      if @syphonServer.hasClients
+        @syphonServer.publishFrameTexture(texture.name,
                         textureTarget: GL_TEXTURE_2D,
                           imageRegion: NSMakeRect(0, 0, texture.width, texture.height),
                     textureDimensions: NSMakeSize(texture.width, texture.height),
                               flipped: true)
+                            
+      end
     end
   end
-end
+ end
